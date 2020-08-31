bb5cc18e72dd09d43f58345976e054bac71bee8a
Ori Shalev <ori.shalev@gmail.com>
2020-08-30 21:14:25 -0700

    Add curling.sh script wrapping grpcurl

---
+++ scripts/curling.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gitroot=$(git rev-parse --show-toplevel)
+
+grpcurl -insecure \
+  -import-path ${gitroot}/api \
+  -import-path /usr/local/Cellar/protobuf/3.13.0/include \
+  -import-path ${gitroot}/vendor \
+  -import-path ${gitroot}/vendor/github.com/gogo/googleapis \
+  -import-path ${gitroot}/vendor/github.com/grpc-ecosystem/grpc-gateway \
+  -emit-defaults \
+  -proto controller.proto localhost:10000 "$@" | jq .
--- scripts/generate-protobuf.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-root=$(git rev-parse --show-toplevel)
-
-cd ${root}/api
-
-protoc --gofast_out=plugins=grpc:. controller.proto
-
-cd - >/dev/null 2>&1
