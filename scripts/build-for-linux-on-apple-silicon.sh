c050e44244adcfff8c916e0417b9315f7798adaa
Ori Shalev <ori.shalev@gmail.com>
2025-09-03 18:59:12 -0700

    Use Go 1.24 in the docker build image

--- scripts/build-for-linux-on-apple-silicon.sh
+++ scripts/build-for-linux-on-apple-silicon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/src -w /src --platform=linux/arm64 golang:1.21 \
+docker run --rm -v $PWD:/src -w /src --platform=linux/arm64 golang:1.24 \
   sh -c 'apt-get update && apt-get install -y gcc libc6-dev && \
          GOARCH=arm64 GOOS=linux CGO_ENABLED=1 go build ./cmd/controller'
 
